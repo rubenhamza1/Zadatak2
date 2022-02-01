8983b420a09b814a888df807b843e38f2de4ce2c
rubenhamza1 <90246098+rubenhamza1@users.noreply.github.com>
2022-02-01 13:07:59 +0100

    Add files via upload

---
+++ Zadatak2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Zadatak2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int god;
+            
+
+
+            for (god = 1900; god < 2101; god++)
+            {
+                if (god % 4 == 0 && god % 100 != 0)
+                {
+                     Console.WriteLine("godina " + god + " je prijestupna godina. ");
+                }
+                if (god % 400 == 0)
+                Console.WriteLine("godina " + god + " je prijestupna godina. ");
+            }
+
+            Console.ReadKey(); 
+        
+        }
+
+
+    }
+}
